4c1153905fc58157ded58874c776e10d6f16dfbc
anwah826 <juluanwah@gmail.com>
2025-06-15 20:00:24 +0100

    Add fixture loading view and URL

--- core/urls.py
+++ core/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('update-cart/<int:item_id>/<str:action>/', views.update_cart_quantity, name='update_cart_quantity'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('load-fixtures/', load_fixtures, name='load_fixtures'),
 ]
